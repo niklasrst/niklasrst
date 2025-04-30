fcf3310624209acceef99415bfc3c813fd4f4cc3
Niklas Rast <177048868+niklasrst@users.noreply.github.com>
2025-04-30 18:26:01 +0200

    fix(dsc): added winget repair

--- setup.ps1
+++ setup.ps1
@@ -48,6 +48,30 @@ $destination = "$ENV:LOCALAPPDATA\Microsoft\Windows\Shell\LayoutModification.xml
 Invoke-WebRequest -Uri $url -OutFile $destination
 
 # Winget DSC
+Function Get-WingetCmd {
+    $WingetCmd = $null
+    #Get WinGet Path
+    try {
+        #Get Admin Context Winget Location
+        $WingetInfo = (Get-Item "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_*_8wekyb3d8bbwe\winget.exe").VersionInfo | Sort-Object -Property FileVersionRaw
+        #If multiple versions, pick most recent one
+        $WingetCmd = $WingetInfo[-1].FileName
+    }
+    catch {
+        #Get User context Winget Location
+        if (Test-Path "$env:LocalAppData\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe") {
+            $WingetCmd = "$env:LocalAppData\Microsoft\WindowsApps\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\winget.exe"
+        }
+    }
+
+    return $WingetCmd
+}
+
+if ($null -eq (Get-WingetCmd)) { 
+    Install-Module -Name Microsoft.WinGet.Client -Force -Confirm:$false
+    Repair-WinGetPackageManager -AllUsers -Force
+}
+
 $url = "https://raw.githubusercontent.com/niklasrst/niklasrst/refs/heads/main/configuration.dsc.yaml"
 $destination = "$env:TEMP\configuration.dsc.yaml"
 Invoke-WebRequest -Uri $url -OutFile $destination
