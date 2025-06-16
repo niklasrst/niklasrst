5aad418ff7d93a73360a6fa08e73a1020a2d582e
Niklas Rast <177048868+niklasrst@users.noreply.github.com>
2025-06-16 21:19:56 +0200

    feat(mac-dsc): homebrew setup

--- setupmac.sh
+++ setupmac.sh
@@ -25,3 +25,6 @@
 ## APPS
 ### HOMEBREW
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo >> /$HOME/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /$HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
