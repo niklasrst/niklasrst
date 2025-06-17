8584b3dc4506f3f1b5949f3e46e293c324d46969
Niklas Rast <177048868+niklasrst@users.noreply.github.com>
2025-06-17 12:31:30 +0200

    feat(mac-dsc): added installer steps

--- setupmac.sh
+++ setupmac.sh
@@ -26,6 +26,7 @@
 ## CONFIG
 #################
 ### DARKMODE
+osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
 
 ### WALLPAPER
 IMAGE_URL="https://raw.githubusercontent.com/niklasrst/niklasrst/refs/heads/main/bg.jpg"
@@ -43,10 +44,14 @@ end tell
 EOD
 done
 
+### CONFIGS FOLDER
+mkdir -p ~/.config
+
 #################
 ## FOLDERS
 #################
 ### REPO
+mkdir -p ~/repos
 
 #################
 ## APPS
@@ -57,15 +62,38 @@ echo >> /$HOME/.zprofile
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /$HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+### GITCREDMGR
+brew install --cask git-credential-manager
+
 ### PWSH
+brew install --cask powershell
+
 #### PWSH MODULES
 ### VSCODE
+#### In VSCODE search for `Shell Command`
+
 ### OhMyPosh
+brew install oh-my-posh
+
 ### KeePass
+brew install --cask keepassxc
+
 ### lazygit
+brew install lazygit
+
+### superfile
+brew install superfile
+
+### WindowsApp
+brew install --cask windows-app
 
 #################
 ## GIT REPOS
 #################
 ### DOTFILES (GITHUB)
+git clone --recurse-submodules https://github.com/niklasrst/dotfiles ~/repos/dotfiles
+ln -s ~/repos/dotfiles/pwsh ~/.config/powershell
+spf -c ~/repos/dotfiles/superfile/config.toml
+
 ### DOCS (DEVOPS)
+git clone --recurse-submodules https://fraport@dev.azure.com/fraport/FrontEnd-Management/_git/docs ~/repos/docs
